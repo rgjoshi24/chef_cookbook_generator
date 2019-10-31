e03680638085df7819a4355ff35bf56352353868
Joshi <RJOSHI1@americredit.com>
2019-10-31 15:35:01 -0500

    changed fqdn

--- cookbooks/code_generator_wrapper/attributes/default.rb
+++ cookbooks/code_generator_wrapper/attributes/default.rb
@@ -1,3 +1,3 @@
 default['code_generator_wrapper']['chef_supermarket_url'] = 'https://supermarket.chef.io'
-default['code_generator_wrapper']['chef_server_fqdn'] = 'https://manage.chef.io'
-default['code_generator_wrapper']['chef_supermarket_fqdn'] = 'https://supermarket.chef.io'
+default['code_generator_wrapper']['chef_server_fqdn'] = 'manage.chef.io'
+default['code_generator_wrapper']['chef_supermarket_fqdn'] = 'supermarket.chef.io'
